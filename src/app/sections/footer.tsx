a69bfd8af02c3b089caf8f8b50f685fb0a86ef8a
guillaume.vacelet <guillaume.vacelet@gmail.com>
2025-02-17 17:38:25 +0900

    fix: removed footer padding

--- src/app/sections/footer.tsx
+++ src/app/sections/footer.tsx
@@ -2,8 +2,8 @@ import SocialLinks from "../components/socialLinks";
 
 export default function Footer() {
   return (
-    <section id="footer" className="w-full p-[10px]">
-      <div className="w-full flex flex-col justify-between gap-6 py-12 px-6 text-xs md:text-base font-medium bg-black rounded-md">
+    <section id="footer" className="w-full">
+      <div className="w-full flex flex-col justify-between gap-6 py-12 px-6 text-xs md:text-base font-medium bg-black">
         <SocialLinks />
         <span className="text-base">guillaume.vacelet@gmail.com</span>
         <span className="text-[10px] sm:text-xs leading-none text-zinc-500">© 2025 Guillaume Vacelet. All rights reserved.</span>
