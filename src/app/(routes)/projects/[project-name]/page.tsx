15c81c810ede42e754b404f672e0c3c61852e6ca
guillaume.vacelet <guillaume.vacelet@gmail.com>
2025-02-21 01:43:01 +0900

    feat: iconChip component for tech stacks

--- src/app/(routes)/projects/[project-name]/page.tsx
+++ src/app/(routes)/projects/[project-name]/page.tsx
@@ -21,14 +21,14 @@ export default function Projects() {
   }, []);
 
   return (
-    <div className="size-full min-h-screen  bg-black">
-      <div className="size-full flex flex-col items-center justify-center px-6">
+    <div className="w-full min-h-screen bg-black">
+      <div className="size-full flex flex-col items-center justify-center px-4 sm:px-6">
         <div className="relative w-full flex flex-row items-center justify-between h-16">
           <Link href="/"
             scroll={false}
             onClick={() => router.back()}
             className="absolute p-0 left-0">
-            <div className="relative size-6 svg-to-white rotate-180">
+            <div className="relative size-4 sm:size-5 md:size-6 svg-to-white rotate-180">
               <Image
                 src={`/static/images/arrow.right.svg`}
                 alt="arrow icon"
@@ -54,7 +54,7 @@ export default function Projects() {
 
         {projectIndex === -1
           ? <div className="w-full h-screen flex items-center justify-center">Loading</div>
-          : <div className="max-w-[800px] flex flex-col gap-8 sm:gap-12 mt-8 sm:mt-16 md:mt-[150px] px-4 sm:px-6 pb-20">
+          : <div className="w-full md:max-w-[800px] flex flex-col justify-start gap-8 sm:gap-12 mt-8 pb-20">
               <div className="flex flex-col gap-2 sm:gap-3 pt-8 sm:pt-12">
                 <span className="text-base sm:text-lg font-medium text-gray-400">{projects[projectIndex].year}</span>
                 <h1 className="text-2xl sm:text-3xl font-semibold text-white">{projects[projectIndex].title}</h1>
@@ -72,7 +72,7 @@ export default function Projects() {
               {/* Tech stack */}
               <div className="flex flex-col gap-4">
                 <h3 className="text-lg sm:text-2xl font-medium text-white">Tech stack</h3>
-                <div className="flex flex-row gap-2">
+                <div className="w-full flex flex-row gap-2 flex-wrap">
                   {projects[projectIndex].techstack.map(tech => 
                     <IconChip key={tech} label={tech} icon={`/static/images/${tech.toLowerCase()}.svg`} />
                   )}
---
+++ src/app/_components/iconChip.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function IconChip({ icon, label } : { icon: string, label: string }) {
+  return (
+    <div className="flex flex-row items-center justify-center gap-2 px-3 py-[6px] rounded-lg border-zinc-800 border-[1px] icon-chip-gradient">
+      <div className="relative size-5 svg-to-gray">
+        <Image
+          src={icon}
+          alt="icon"
+          fill
+          style={{objectFit: "cover"}}
+        />
+      </div>
+      <span className="text-base font-normal leading-none text-zinc-400">
+        {String(label).charAt(0).toUpperCase() + String(label).slice(1)}
+      </span>
+    </div>
+  )
+}
