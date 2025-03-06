4fb24f41496e23a80b452c8ef095925c5bd67145
guillaume.vacelet <guillaume.vacelet@gmail.com>
2025-03-06 14:09:33 +0900

    feat: added i18n support for ProjectSection

--- src/app/_components/projectCard.tsx
+++ src/app/_components/projectCard.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
 import {ProjectInfos} from "@/app/_data/projects";
+import { useTranslations } from "next-intl";
 
 export default function ProjectCard({project} : {project: ProjectInfos}) {
+  const t = useTranslations(`homePage.projectsSection.${project.title.toLowerCase()}`);
+  
   return (
     <Link href={`/projects/${project.title.toLowerCase()}`} className="w-full flex flex-col group bg-white rounded-2xl border-cardBorder border-[0.35px] shadow-sm card-hover-style hover:shadow-md">
       <div className="relative rounded-t-2xl overflow-hidden">
@@ -18,8 +21,8 @@ export default function ProjectCard({project} : {project: ProjectInfos}) {
 
       <div className="flex flex-col justify-between text-xs md:text-base font-medium p-4 gap-3">
         <div className="flex flex-col">
-          <span className="text-xl font-medium text-black">{project.title}</span>
-          <p className="text-sm font-normal text-gray-600">{project.subtitle}</p>
+          <span className="text-xl font-medium text-black">{t('title')}</span>
+          <p className="text-sm font-normal text-gray-600">{t('desc')}</p>
         </div>
 
         {/* <div className="w-fit flex flex-row items-center gap-2 text-sm text-accent group-hover:gap-3 transition-all duration-300">
--- src/app/_sections/projects.tsx
+++ src/app/_sections/projects.tsx
@@ -1,11 +1,14 @@
 import ProjectCard from "@/app/_components/projectCard";
 import projects from "@/app/_data/projects";
+import { useTranslations } from "next-intl";
 
 export default function ProjectsSection() {
+  const t = useTranslations("homePage.projectsSection");
+
   return (
     <section id="projects" className="section flex-col items-center px-4 sm:px-12 md:px-20 xl:px-40 py-16">
       <div className="w-full flex flex-col items-start gap-8">
-        <h2 className="text-black text-5xl font-medium">Key projects</h2>
+        <h2 className="text-black text-5xl font-medium">{t('title')}</h2>
 
         <div className="w-full grid grid-cols-1 sm:grid-cols-2 gap-6">
           {projects.map(project =>
