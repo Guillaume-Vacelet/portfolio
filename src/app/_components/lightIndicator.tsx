d7f6966fecaf92e7865b985e05b3375faa2cb7b4
guillaume.vacelet <guillaume.vacelet@gmail.com>
2025-03-12 18:20:32 +0900

    refactor: wrapped all code in same component file

--- src/app/_components/lightIndicator.tsx
+++ src/app/_components/lightIndicator.tsx
@@ -1,8 +1,15 @@
+import { useTranslations } from "next-intl";
+
 export default function LightIndicator() {
+  const t = useTranslations('homePage.homeSection');
+  
   return (
-    <div className="relative flex-shrink-0 size-3">
-      <div className="absolute top-0 left-0 size-full rounded-full bg-green-500 opacity-80 animate-ping"></div>
-      <div className="size-full rounded-full bg-green-500 shadow-[0px_0px_20px_4px_#48bb78]"></div>
+    <div className="mb-8 w-fit flex flex-row items-center gap-2 py-1 px-2 md:px-3 rounded-full border bg-black border-gray-700">
+      <div className="relative flex-shrink-0 size-3">
+        <div className="absolute top-0 left-0 size-full rounded-full bg-green-500 opacity-80 animate-ping"></div>
+        <div className="size-full rounded-full bg-green-500 shadow-[0px_0px_20px_4px_#48bb78]"></div>
+      </div>
+      <span className="text-xs sm:text-sm leading-none mt-[2px]">{t('status')}</span>
     </div>
   )
 }
