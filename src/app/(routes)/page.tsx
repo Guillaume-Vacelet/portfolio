d914927ac397f159953c08e4d5531c6ede75a603
guillaume.vacelet <guillaume.vacelet@gmail.com>
2025-12-17 21:33:25 +0900

    🚧 Add the work-in-progress screen

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -9,13 +9,19 @@ import NavbarModal from "@/app/_components/navbarModal";
 export default function Home() {
 
   return (
-    <div className="font-nuckle w-full h-full flex flex-col items-center relative">
-      <Navbar hideOnScroll />
-      <NavbarModal />
-      <HomeSection />
-      <AboutSection />
-      <ProjectsSection />
-      <Footer />
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className="px-8 py-8 border-2 border-black flex flex-col items-center gap-4">
+        <h1 className="text-black text-2xl font-semibold">🚧 Website under development</h1>
+        <h2 className="text-black text-sm">📩 You can contact me at guillaume.vacelet@gmail.com</h2>
+      </div>
     </div>
+    // <div className="font-nuckle w-full h-full flex flex-col items-center relative">
+    //   <Navbar hideOnScroll />
+    //   <NavbarModal />
+    //   <HomeSection />
+    //   <AboutSection />
+    //   <ProjectsSection />
+    //   <Footer />
+    // </div>
   );
 }
